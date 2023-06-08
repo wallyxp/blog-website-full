6cce554794dcf9e178f4a16cd1a98b15273e71ea
Debjyoti Nandi <56500077+wallyxp@users.noreply.github.com>
2023-06-08 17:00:24 +0530

    Update Single.jsx

--- client/src/pages/single/Single.jsx
+++ client/src/pages/single/Single.jsx
@@ -7,7 +7,7 @@ export default function Single() {
     return (
         <div className='single'>
             <SinglePost/>
-            <SIdebar/>
+//             <SIdebar/>
         </div>
     )
 }
